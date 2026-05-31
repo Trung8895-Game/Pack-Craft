c6be586f67232b08da40ce5f7672b1d6eeb0fdbf
trung881995 <trung.pham8895@gmail.com>
2026-05-31 22:47:22 +0700

    Add ItemSaveData

---
+++ Assets/Scripts/Gameplay/Inventory/ItemSaveData.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class ItemSaveData
+{
+    public string ItemId;
+
+    public Vector2Int Position;
+
+    public RotationState Rotation;
+}
